fda2d3e55ccd9d88655ed5157877e54edc8fae23
Maxwell Moss <159182842+Maxwelltheguy@users.noreply.github.com>
2024-10-16 11:21:07 -0400

    Added a Main Menu

---
+++ Camera Idea/Assets/Scripts/Button Events.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonEvents : MonoBehaviour
+{
+    [SerializeField] string tutorialScene;
+
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.None;
+    }
+
+    public void EnterTutorial()
+    {
+        SceneManager.LoadScene(tutorialScene);
+    }
+
+}
--- Camera Idea/Assets/Scripts/Level End.cs
+++ Camera Idea/Assets/Scripts/Level End.cs
@@ -1,18 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
+
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LevelEnd : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] string nextLevel;
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter(Collider collision)
     {
-        
+        if (collision.CompareTag("Player"))
+        {
+            SceneManager.LoadScene(nextLevel);
+        }
     }
 }
