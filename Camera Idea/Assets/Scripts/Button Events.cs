7e6d98b29ff53e76520ebe8cf905d439a5623dc0
Maxwell Moss <159182842+Maxwelltheguy@users.noreply.github.com>
2024-11-05 11:18:09 -0500

    Started First Level

--- Camera Idea/Assets/Scripts/Button Events.cs
+++ Camera Idea/Assets/Scripts/Button Events.cs
@@ -7,15 +7,21 @@ using UnityEngine.SceneManagement;
 public class ButtonEvents : MonoBehaviour
 {
     [SerializeField] string tutorialScene;
+    [SerializeField] string firstLevel;
 
     private void Start()
     {
         Cursor.lockState = CursorLockMode.None;
     }
 
+    //Declaring the events for the buttons
     public void EnterTutorial()
     {
         SceneManager.LoadScene(tutorialScene);
+    }
+    public void StartGame()
+    {
+        SceneManager.LoadScene(firstLevel);
     }
 
     public void ExitGame()
